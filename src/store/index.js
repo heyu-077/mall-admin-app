988cde5218f0f1ae71afc7ee113b38303f3d1b85
heyu-077 <1834617305@qq.com>
2020-12-10 23:57:14 +0800

    系统初始布局代码优化

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // false菜单不闭合 true闭合
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
---
+++ src/views/layout/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrump">
+      <a-breadcrumb>
+        <a-breadcrumb-item>商品</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">商品列表</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="use-info">
+      <li>欢迎hy-077 <a-icon type="down" /></li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
