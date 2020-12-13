c095c919e957fca08249d08c57fbc65f984ab533
heyu-077 <1834617305@qq.com>
2020-12-14 00:15:54 +0800

    实现菜单栏的切换和路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -3,27 +3,78 @@ import VueRouter from 'vue-router';
 import store from '@/store/index';
 import Home from '../views/layout/Home.vue';
 import Login from '../views/layout/Login.vue';
+import getMenuRouter from '../utils/permission';
 
 Vue.use(VueRouter);
+
+const asyncRouterMap = [{
+  path: '/product',
+  name: 'Product',
+  component: Home,
+  meta: {
+    title: '商品',
+    hidden: false,
+    icon: 'inbox',
+  },
+  children: [{
+    path: 'list',
+    name: 'ProductList',
+    meta: {
+      title: '商品列表',
+      hidden: false,
+      icon: 'unordered-list',
+    },
+    component: () => import('../views/pages/productList.vue'),
+  }, {
+    path: 'add',
+    name: 'ProductAdd',
+    meta: {
+      title: '添加商品',
+      hidden: false,
+      icon: 'shopping-cart',
+    },
+    component: () => import('../views/pages/productAdd.vue'),
+  }, {
+    path: 'category',
+    name: 'Category',
+    meta: {
+      title: '类目管理',
+      hidden: false,
+      icon: 'setting',
+    },
+    component: () => import('../views/pages/category.vue'),
+  }],
+}];
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: '首页',
+      hidden: false,
+      icon: 'home',
+    },
+    children: [{
+      path: 'index',
+      name: 'Index',
+      meta: {
+        title: '统计',
+        hidden: false,
+        icon: 'line-chart',
+      },
+      component: () => import('../views/pages/index.vue'),
+    }],
   },
   {
     path: '/login',
-    name: Login,
+    name: 'Login',
+    meta: {
+      title: '登录',
+      hidden: true,
+    },
     component: Login,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
@@ -31,15 +82,23 @@ const router = new VueRouter({
   routes,
 });
 
+let isAddRoutes = false;
 router.beforeEach((to, from, next) => {
   if (to.path !== '/login') {
     if (store.state.user.username && store.state.user.appkey && store.state.user.role) {
-      next();
-    } else {
-      next('/login');
+      if (!isAddRoutes) {
+        const menuRoutes = getMenuRouter(store.state.user.role, asyncRouterMap);
+        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes)).then(() => {
+          router.addRoutes(menuRoutes);
+          next();
+        });
+        isAddRoutes = true;
+      }
+      return next();
     }
+    return next('/login');
   }
-  next();
+  return next();
 });
 
 export default router;
---
+++ src/utils/permission.js
@@ -0,0 +1,40 @@
+/**
+ * 用户角色对应的权限名称
+ */
+const roleToRoute = {
+  coustomer: [{
+    name: 'Product',
+  }, {
+    name: 'ProductList',
+  }, {
+    name: 'ProductAdd',
+  }],
+  admit: [{
+    name: 'Product',
+  }, {
+    name: 'ProductList',
+  }, {
+    name: 'ProductAdd',
+  }, {
+    name: 'Category',
+  }],
+};
+
+/**
+ * 过滤没有权限的路由
+ * @param {*} role
+ * @param {*} routes
+ */
+export default function getMenuRouter(role, routes) {
+  const allowRoutesName = roleToRoute[role].map((item) => item.name);
+  const resultRoutes = routes.filter((r) => {
+    const obj = r;
+    if (allowRoutesName.indexOf(r.name) !== -1) {
+      const { children } = obj;
+      obj.children = children.filter((c) => allowRoutesName.indexOf(c.name) !== -1);
+      return true;
+    }
+    return false;
+  });
+  return resultRoutes;
+}
