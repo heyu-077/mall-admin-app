3617de73500f48e51c5cbd7d487ddd707b9c26c7
heyu-077 <1834617305@qq.com>
2020-12-14 16:58:49 +0800

    实现统计页面

--- src/views/pages/index.vue
+++ src/views/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-    <div>统计</div>
+  <div class="index">
+    <div class="card">
+       <card />
+       <charts />
+    </div>
+  </div>
 </template>
+
+<script>
+import Card from '@/components/card.vue';
+import Charts from '@/components/charts.vue';
+
+export default {
+  components: {
+    Card,
+    Charts,
+  },
+};
+</script>
