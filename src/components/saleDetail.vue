cf4096bf5d445f3200c287d148757c561fcdf010
heyu-077 <1834617305@qq.com>
2020-12-21 22:21:32 +0800

    实现新增商品页面和销售信息的填写

---
+++ src/components/saleDetail.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="sale-detail">
+    <a-form-model
+      ref="ruleForm"
+      :model="form"
+      :rules="rules"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+    >
+      <a-form-model-item label="商品售价" prop="price" required>
+        <a-input v-model="form.price" />
+      </a-form-model-item>
+      <a-form-model-item label="折扣价格" prop="price_off">
+        <a-input v-model="form.price_off" />
+      </a-form-model-item>
+      <a-form-model-item label="商品库存" prop="inventory" required>
+        <a-input v-model="form.inventory" />
+      </a-form-model-item>
+      <a-form-model-item label="计量单位" prop="unit" required>
+        <a-input v-model="form.unit" />
+      </a-form-model-item>
+      <a-form-model-item label="商品相册" prop="images">
+        <a-upload
+          :action="
+            'https://mallapi.duyiedu.com/upload/images?appkey=' +
+            $store.state.user.appkey
+          "
+          list-type="picture-card"
+          :file-list="fileList"
+          @preview="handlePreview"
+          @change="handleChange"
+          name="avatar"
+        >
+          <div v-if="fileList.length < 8">
+            <a-icon :type="loading ? 'loading' : 'plus'" />
+            <div class="ant-upload-text">Upload</div>
+          </div>
+          <a-modal
+            :visible="previewVisible"
+            :footer="null"
+            @cancel="handleCancel"
+          >
+            <img alt="example" style="width: 100%" :src="previewImage" />
+          </a-modal>
+        </a-upload>
+      </a-form-model-item>
+      <a-form-model-item label="步骤" class="prev-btn">
+        <a-button @click="prev">上一步</a-button>
+        <a-button type="primary" @click="next">提交</a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+<script>
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
+export default {
+  data() {
+    return {
+      previewVisible: false,
+      previewImage: '',
+      labelCol: { span: 5 },
+      wrapperCol: { span: 12 },
+      rules: {},
+      loading: false,
+      fileList: [],
+    };
+  },
+  methods: {
+    async handlePreview(file) {
+      const obj = file;
+      if (!file.url && !file.preview) {
+        obj.preview = await getBase64(file.originFileObj);
+      }
+      this.previewImage = file.url || file.preview;
+      this.previewVisible = true;
+    },
+    handleCancel() {
+      this.previewVisible = false;
+    },
+    handleChange({ file, fileList, event }) {
+      console.log('change:', file, fileList, event);
+      this.fileList = fileList;
+    },
+    next() {
+      this.$emit('next', this.form);
+    },
+    prev() {
+      this.$emit('prev');
+    },
+  },
+  props: ['form'],
+};
+</script>
--- src/views/pages/productAdd.vue
+++ src/views/pages/productAdd.vue
@@ -1,3 +1,81 @@
 <template>
-    <div>添加商品</div>
+  <div class="product-detail">
+    <a-steps :current="current"  class="product-steps">
+      <a-step v-for="item in steps" :key="item.title" :title="item.title" />
+    </a-steps>
+    <div class="steps-content">
+      <base-info v-if="current === 0" @next="next" :form="form" />
+      <sale-info v-if="current === 1" @next="next" @prev="prev" :form="form" />
+    </div>
+  </div>
 </template>
+<script>
+import baseInfo from '@/components/baseDetail.vue';
+import saleInfo from '@/components/saleDetail.vue';
+
+export default {
+  data() {
+    return {
+      current: 0,
+      steps: [
+        {
+          title: '填写商品基本信息',
+        },
+        {
+          title: '填写商品销售信息',
+        },
+      ],
+      form: {
+        title: '',
+        desc: '',
+        category: '',
+        c_items: '',
+        tags: '',
+        price: 0,
+        price_off: 0,
+        unit: '',
+        inventory: 0,
+        images: [],
+      },
+    };
+  },
+  components: {
+    baseInfo,
+    saleInfo,
+  },
+  methods: {
+    next(form) {
+      this.form = {
+        ...this.form,
+        form,
+      };
+      if (this.current === 1) {
+        // 提交数据
+      } else {
+        this.current += 1;
+      }
+    },
+    prev() {
+      this.current -= 1;
+    },
+  },
+};
+</script>
+<style lang='less' scoped>
+.product-detail{
+  .product-steps{
+    width: 50%;
+    margin: 20px auto;
+  }
+   .steps-content {
+  margin-top: 16px;
+  border: 1px dashed #e9e9e9;
+  border-radius: 6px;
+  background-color: #fafafa;
+  min-height: 200px;
+  text-align: center;
+  padding-top: 80px;
+  }
+}
+
+</style>
